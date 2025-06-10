16c03bad3b1e77d0cb06157266be9b1e87584a44
jhonatanreistavares <tavares-jr2014@hotmail.com>
2025-06-10 11:51:42 -0400

    Camera Working
    scrip da câmera como campos para personalização dos valores para ela

---
+++ Cenas/Player/camera_spring_arm.gd
@@ -0,0 +1,33 @@
+extends Node3D
+
+@export var mouse_sensibility: float = 0.0005
+@export_range(-90.0, 0.0, 0.1, "radians as degress") var min_vertical_angle: float = -PI/2
+@export_range(0.0, 90.0, 0.1, "radians as degress") var max_vertical_angle: float = PI/4
+
+@onready var spring_arm := $SpringArm3D
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotation.y -= event.relative.x * mouse_sensibility
+		rotation.y = wrapf(rotation.y, 0.0, TAU)
+		
+		rotation.x -= event.relative.y * mouse_sensibility
+		rotation.x = clamp(rotation.x, -PI/2, PI/4)
+		
+		
+	if event.is_action_pressed("wheel_up"):
+		spring_arm.spring_length -= 1
+	if event.is_action_pressed("wheel_down"):
+		spring_arm.spring_length += 1
+	
+	if event.is_action_pressed("toggle_mouse_capture"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			 
